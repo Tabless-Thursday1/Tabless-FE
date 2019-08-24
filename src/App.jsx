2fa8e1ecc023924f46a1d6ae2252e3d37d875e63
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-24 16:42:48 -0400

    added dependencies, setup eslint, setup SMACSS, created example component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+
+import './App.scss';
+
 import logo from './logo.svg';
-import './App.css';
+import ExampleComponent from './components/ExampleComponent';
 
 function App() {
   return (
@@ -8,7 +11,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit&nbsp;
+          <code>src/App.js</code>
+          &nbsp;and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,6 +23,7 @@ function App() {
         >
           Learn React
         </a>
+        <ExampleComponent />
       </header>
     </div>
   );
---
+++ src/components/ExampleComponent/ExampleComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './ExampleComponent.scss';
+
+function ExampleComponent() {
+  return (
+    <>
+      <p className="example-component">Example Component</p>
+    </>
+  );
+}
+
+export default ExampleComponent;

7a146ab9c68fae7c17547915327eba33968fe545
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-25 00:29:51 -0400

    changed styling of buildMapStatetoProps

--- src/utils/buildMapStateToProps.js
+++ src/utils/buildMapStateToProps.js
@@ -1,8 +1,8 @@
 import { filterObject } from './filterObject';
 
-export const buildMapStateToProps = ({ propsShape }) => (state) => (filterObject({
+export const buildMapStateToProps = ({ propsShape }) => (state) => filterObject({
   sourceObject: state,
   filter: propsShape,
-}));
+});
 
 export default buildMapStateToProps;

9b2ef2d207b9d773109276476911184bc0fdafab
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-25 00:29:00 -0400

    changed styling of filterObject

--- src/utils/filterObject.js
+++ src/utils/filterObject.js
@@ -1,10 +1,6 @@
-export function filterObject({ sourceObject, filter }) {
-  return (Object
-    .keys(filter)
-    .reduce((acc, key) => {
-      acc[key] = sourceObject[key];
-      return acc;
-    }, {}));
-}
+export const filterObject = ({ sourceObject, filter }) => Object.keys(filter).reduce((acc, key) => {
+  acc[key] = sourceObject[key];
+  return acc;
+}, {});
 
 export default filterObject;

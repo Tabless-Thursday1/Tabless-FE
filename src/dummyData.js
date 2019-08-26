9ddcc0815be240dad98991c1e75f473eb9b0aabe
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-26 13:32:08 -0400

    added dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,13 @@
+export const dummyData = [
+  {
+    url: 'www.google.com',
+    private: false,
+    description: "it's google",
+    categories: [
+      'search',
+      'research',
+    ],
+  },
+];
+
+export default dummyData;

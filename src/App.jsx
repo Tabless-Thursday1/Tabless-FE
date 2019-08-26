17fa0f27bc4fd01c2da5ea23636b939878a43c94
Ben <bigriffin94@outlook.com>
2019-08-26 14:37:19 -0700

    App.jsx Corrected Imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import './App.css';
-import { Route } from "react-router-dom";
-import HomePage from "./components/NavBar_Tabs/HomePage/home_page";
-import Tab1 from "./components/NavBar_Tabs/Tab1/tab1";
-import Tab2 from "./components/NavBar_Tabs/Tab2/tab2";
-import Tab3 from "./components/NavBar_Tabs/Tab3/tab3";
-import Tab4 from "./components/NavBar_Tabs/Tab4/tab4";
-
-function App() {
-  return (
-    <div className="App">
-    <NavTab/>
-      <Route exact path="/" component={HomePage}/>
-=======
 import './App.scss';
 import logo from './logo.svg';
 import Home from "./components/Home/";
@@ -36,7 +21,6 @@ function App() {
       <NavTab/>
       <Home />
       <Route exact path="/" component={WelcomePage}/>
->>>>>>> f3d62f0f539d7cc5cf02722f5eafafbd3b9924b2
       <Route path="/tab1" component={Tab1}/>
       <Route path="/tab2" component={Tab2}/>
       <Route path="/tab3" component={Tab3}/>

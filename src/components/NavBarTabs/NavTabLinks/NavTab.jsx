78d94a2d98b27303829b0299a7e5ddf54c95c8f2
Ben <bigriffin94@outlook.com>
2019-08-27 13:31:54 -0700

    Styling added to NavTabBar

--- src/components/NavBarTabs/NavTabLinks/NavTab.jsx
+++ src/components/NavBarTabs/NavTabLinks/NavTab.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import { Tabs, Icon } from 'antd';
 
 import './NavTab.scss';
 
@@ -14,17 +14,22 @@ function NavTab() {
   return (
     <div className="nav-wrapper">
       <Tabs defaultActiveKey="1" onChange={callback}>
-        <TabPane tab="Welcome" key="1">
-          <h3>Welcome to Tabless Thursday</h3>
+        <TabPane tab="Your Tabs" key="1" className="your-tabs your">
+          <h3>We can route our tabs here.</h3>
         </TabPane>
-        <TabPane tab="About" key="2">
+        <TabPane tab="Welcome" key="2" className="welcome-tab welcome">
+          <h3>Welcome to Tabless Thursday</h3>
+          <p>Here at Tabless Thursday we strive to make your browser look and feel more organized.</p>
+        </TabPane>
+        <TabPane tab="About" key="3" className="about-tab about">
+          <h3>About Tabless Thursday</h3>
           <p>
             People often have a million tabs up and lose track of what they were doing.
             This website lets you organize them in to-do lists and make a note of why it was important.
             Makes it easy to pick up where you left off.
           </p>
         </TabPane>
-        <TabPane tab="Meet The Team" key="3">
+        <TabPane tab="Meet The Team" key="4">
           <div className="meet-the-team team">
             <h2>WEB UI DEVELOPER</h2>
             <ul>

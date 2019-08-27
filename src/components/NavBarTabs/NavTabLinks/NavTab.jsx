3a505d589c54729859007ae51e5e40b02891b610
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-26 21:18:54 -0400

    linting NavTab

--- src/components/NavBarTabs/NavTabLinks/NavTab.jsx
+++ src/components/NavBarTabs/NavTabLinks/NavTab.jsx
@@ -3,17 +3,25 @@ import { NavLink } from 'react-router-dom';
 import { Icon } from 'antd';
 
 
-function NavTab() {
-  return (
-    <div>
-      <div className="nav-wrapper wrapper">
-        <NavLink exact to="/" activeClassName="tab welcome"><h1>Tabless Thursday <Icon type="folder-open" theme="twoTone"/></h1></NavLink>
-        <NavLink to="/tab1" activeClassName="tab">Tab 1</NavLink>
-        <NavLink to="/tab2" activeClassName="tab">Tab 2</NavLink>
-        <NavLink to="/tab3" activeClassName="tab">Tab 3</NavLink>
-        <NavLink to="/tab4" activeClassName="tab">Tab 4</NavLink>
-      </div>
+const NavTab = () => (
+  <div>
+    <div className="nav-wrapper wrapper">
+      <NavLink
+        exact
+        to="/"
+        activeClassName="tab welcome"
+      >
+        <h1>
+          {'Tabless Thursday '}
+          <Icon type="folder-open" theme="twoTone" />
+        </h1>
+      </NavLink>
+      <NavLink to="/tab1" activeClassName="tab">Tab 1</NavLink>
+      <NavLink to="/tab2" activeClassName="tab">Tab 2</NavLink>
+      <NavLink to="/tab3" activeClassName="tab">Tab 3</NavLink>
+      <NavLink to="/tab4" activeClassName="tab">Tab 4</NavLink>
     </div>
-  )
-}
+  </div>
+);
+
 export default NavTab;

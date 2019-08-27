b7095cd6c2ef081283168077c89b391180a44674
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-27 13:47:49 -0400

    filled out required actions and thunks, scaffolded out reducers

--- src/reducers/tabsReducer.js
+++ src/reducers/tabsReducer.js
@@ -1,48 +1,111 @@
 import {
-  GET_DATA_START,
-  GET_DATA_SUCCESS,
-  GET_DATA_FAILURE,
-  GET_DATA_WITH_AUTH_START,
-  GET_DATA_WITH_AUTH_SUCCESS,
-  GET_DATA_WITH_AUTH_FAILURE,
   EXAMPLE_TYPE,
+  POST_LOGIN_START,
+  POST_LOGIN_SUCCESS,
+  POST_LOGIN_FAILURE,
+  POST_SIGNUP_START,
+  POST_SIGNUP_SUCCESS,
+  POST_SIGNUP_FAILURE,
+  GET_TABS_START,
+  GET_TABS_SUCCESS,
+  GET_TABS_FAILURE,
+  ADD_TAB_START,
+  ADD_TAB_SUCCESS,
+  ADD_TAB_FAILURE,
+  UPDATE_TAB_START,
+  UPDATE_TAB_SUCCESS,
+  UPDATE_TAB_FAILURE,
+  REMOVE_TAB_START,
+  REMOVE_TAB_SUCCESS,
+  REMOVE_TAB_FAILURE,
 } from '../actions';
+
+import { dummyData } from '../dummyData';
 
 const initialState = {
   exampleVariable: 'Example',
+  tabs: dummyData,
 };
 
 export const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case GET_DATA_START:
-      return {
-        ...state,
-      };
-    case GET_DATA_SUCCESS:
-      return {
-        ...state,
-      };
-    case GET_DATA_FAILURE:
-      return {
-        ...state,
-      };
-    case GET_DATA_WITH_AUTH_START:
-      return {
-        ...state,
-      };
-    case GET_DATA_WITH_AUTH_SUCCESS:
-      return {
-        ...state,
-      };
-    case GET_DATA_WITH_AUTH_FAILURE:
-      return {
-        ...state,
-      };
     case EXAMPLE_TYPE:
       return {
         ...state,
         exampleVariable: payload,
       };
+    case POST_LOGIN_START:
+      return {
+        ...state,
+      }
+    case POST_LOGIN_SUCCESS:
+      return {
+        ...state,
+      }
+    case POST_LOGIN_FAILURE:
+      return {
+        ...state,
+      }
+    case POST_SIGNUP_START:
+      return {
+        ...state,
+      }
+    case POST_SIGNUP_SUCCESS:
+      return {
+        ...state,
+      }
+    case POST_SIGNUP_FAILURE:
+      return {
+        ...state,
+      }
+    case GET_TABS_START:
+      return {
+        ...state,
+      }
+    case GET_TABS_SUCCESS:
+      return {
+        ...state,
+      }
+    case GET_TABS_FAILURE:
+      return {
+        ...state,
+      }
+    case ADD_TAB_START:
+      return {
+        ...state,
+      }
+    case ADD_TAB_SUCCESS:
+      return {
+        ...state,
+      }
+    case ADD_TAB_FAILURE:
+      return {
+        ...state,
+      }
+    case UPDATE_TAB_START:
+      return {
+        ...state,
+      }
+    case UPDATE_TAB_SUCCESS:
+      return {
+        ...state,
+      }
+    case UPDATE_TAB_FAILURE:
+      return {
+        ...state,
+      }
+    case REMOVE_TAB_START:
+      return {
+        ...state,
+      }
+    case REMOVE_TAB_SUCCESS:
+      return {
+        ...state,
+      }
+    case REMOVE_TAB_FAILURE:
+      return {
+        ...state,
+      }
     default:
       if (state === initialState) return state;
       throw new Error(`${type} is not a valid type`);

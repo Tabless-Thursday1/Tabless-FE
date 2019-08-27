ec35aef08abadf6fb32fc9471cfa1f06d6de8a00
Jordan <jordandoan@hotmail.com>
2019-08-27 09:14:29 -0700

    Linting

--- src/components/Home/Group.jsx
+++ src/components/Home/Group.jsx
@@ -1,20 +1,17 @@
-
-const group = (tabs) => {
-  return tabs.reduce((groupedCategories, tab) => {
-    tab.categories.forEach((category) => {
-      const foundIndex = groupedCategories
-        .findIndex((indexedCats) => indexedCats.name === category);
-      if (foundIndex >= 0) {
-        groupedCategories[foundIndex].tabs.push(tab);
-      } else {
-        groupedCategories.push({
-          name: category,
-          tabs: [tab],
-        });
-      }
-    });
-    return groupedCategories;
-  }, []);
-};
+const group = (tabs) => tabs.reduce((groupedCategories, tab) => {
+  tab.categories.forEach((category) => {
+    const foundIndex = groupedCategories
+      .findIndex((indexedCats) => indexedCats.name === category);
+    if (foundIndex >= 0) {
+      groupedCategories[foundIndex].tabs.push(tab);
+    } else {
+      groupedCategories.push({
+        name: category,
+        tabs: [tab],
+      });
+    }
+  });
+  return groupedCategories;
+}, []);
 
 export default group;

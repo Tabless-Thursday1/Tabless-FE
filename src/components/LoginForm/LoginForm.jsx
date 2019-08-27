bc5f756a7efcf636b08db740ad78b134eff6b089
Jordan <jordandoan@hotmail.com>
2019-08-27 13:24:46 -0700

    Added style to LoginForm, with minimal changes to HTML. Added font import to index.scss

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -6,26 +6,31 @@ import * as Yup from 'yup';
 import './LoginForm.scss';
 
 const LoginForm = ({ errors, touched }) => (
-  <div className="Login-Form">
-    <Form>
-      <Field
-        type="text"
-        name="username"
-        placeholder="username"
-      />
-      {touched.username && errors.username && (
-        <p className="error">{errors.username}</p>
-      )}
-      <Field
-        type="password"
-        name="password"
-        placeholder="password"
-      />
-      {touched.password && errors.password && (
-        <p className="error">{errors.password}</p>
-      )}
-      <button type="submit">Submit</button>
-    </Form>
+  <div className="container">
+    <div className="Login-Form">
+      <h2>Welcome Back</h2>
+      <Form>
+        <Field
+          className="field"
+          type="text"
+          name="username"
+          placeholder="Username"
+        />
+        {touched.username && errors.username && (
+          <p className="error">{errors.username}</p>
+        )}
+        <Field
+          className="field"
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
+        <button type="submit" className="ant-btn ant-btn-primary ant-btn-lg">Log In</button>
+      </Form>
+    </div>
   </div>
 );
 
@@ -41,8 +46,8 @@ const FormikLoginForm = withFormik({
   validateOnBlur: false,
 
   validationSchema: Yup.object().shape({
-    username: Yup.string().required(),
-    password: Yup.string().required(),
+    username: Yup.string().required("Username is a required field."),
+    password: Yup.string().required("Password is a required field."),
   }),
 
   handleSubmit(values, { setStatus }) {
